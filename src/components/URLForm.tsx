f6cb0a57d027ba697657449e87c7b2ee2dd6db36
Agustin Acosta <agustinacostaolivera@gmail.com>
2023-07-24 17:00:32 +0200

    Add fetching functionality
    - Imported axios library and the useState and useCallback from the react library to the URLForm component
    
    - Created a const to handle the url state change using reacts built in useState function that tracks the state of a variable.
    
    - Created const handleSubmit that will handle the API call with axios and stores the data in a variable called "result".

--- src/components/URLForm.tsx
+++ src/components/URLForm.tsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState, useCallback } from "react";
+import axios from "axios";
 
 const URLForm: React.FC = () => {
-  return (
-    <div role='container' className='container'>
-        <h1 className='title'>URL SHORTENER</h1>
-        <form className='form-container'>
-            <input className='url-input' type="text"  placeholder='https://example.com' />
-            <button className='button' type='submit'>CREATE SHORT URL</button>
-        </form>
-    </div>
-  )
-}
+  const [url, setUrl] = useState("");
 
-export default URLForm
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      try {
+        const response = await axios.get(
+          `https://api.shrtco.de/v2/shorten?url=${url}`
+        );
+        const result = response.data;
+        console.log(result);
+      } catch (error) {
+        console.log("Error shortening URL:", error);
+      }
+    },
+    [url]
+  );
+  return (
+    <div role="container" className="container">
+      <h1 className="title">URL SHORTENER</h1>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <input
+          className="url-input"
+          type="text"
+          value={url}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setUrl(e.target.value)
+          }
+          placeholder="https://example.com"
+        />
+        <button className="button" type="submit">
+          CREATE SHORT URL
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default URLForm;
