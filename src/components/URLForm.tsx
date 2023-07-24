68696f058da069c9d45e0dac7f522a90e3a267fe
Agustin Acosta <agustinacostaolivera@gmail.com>
2023-07-24 16:38:36 +0200

    Add form element and styles
    - Added form element with an input element with a custom placeholder so the user can paste the URL they want to shorten.
    
    - Added button element to handle the submit of the URL.
    
    - Styled the form, input and button element.

--- src/components/URLForm.tsx
+++ src/components/URLForm.tsx
@@ -4,6 +4,10 @@ const URLForm: React.FC = () => {
   return (
     <div role='container' className='container'>
         <h1 className='title'>URL SHORTENER</h1>
+        <form className='form-container'>
+            <input className='url-input' type="text"  placeholder='https://example.com' />
+            <button className='button' type='submit'>CREATE SHORT URL</button>
+        </form>
     </div>
   )
 }
